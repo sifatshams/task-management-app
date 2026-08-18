225e0ddbe5870e8de50fe27814e078e000967bcb
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-18 06:45:09 +0600

    feat: implemented protect middleware logic successfully

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,38 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model';
+
+// create an middleware for protect routes
+export const protect = async (req, res, next) => {
+  let token;
+
+  if (req.cookies && req.cookies.token) {
+    token = req.cookies.token;
+  } else if (
+    req.headers.authorization &&
+    req.headers.authorization.statsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1]; // extract token
+  }
+
+  // if token not found
+  if (!token) {
+    return res
+      .status(401)
+      .json({ success: false, message: 'Not authorized, user not found!' });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select('-password');
+
+    // if the token is correct but that user is deleted from database
+    if (!req.user) {
+      return res
+        .status(401)
+        .json({ success: false, message: 'User no longer exists!' });
+    }
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ success: false, message: 'Token is invalid or expired!' });
+  }
+};
