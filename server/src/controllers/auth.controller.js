abcef9c0fa901d9913b44b9c22374535ccbdaa1b
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-18 06:31:38 +0600

    feat: added register login get user and update user func. just added func wright now this func are empty

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+// register user
+export const regiterUser = async (req, res) => {};
+
+// login user
+export const loginUser = async (req, res) => {};
+
+// * private
+// get user profile
+export const getUserProfile = async (req, res) => {};
+
+// update user profile
+export const updateUserProfile = async (req, res) => {};
