d788144b9811f0e65dda0c8e3369d254e68dbd35
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-19 10:07:46 +0600

    feat: implement update profile logic

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -40,7 +40,7 @@ export const registerUser = async (req, res) => {
     res.status(201).json({
       success: true,
       message: 'User registered successfully!',
-      data: {
+      user: {
         _id: user._id,
         name: user.name,
         email: user.email,
@@ -124,4 +124,60 @@ export const getUserProfile = async (req, res) => {
 };
 
 // update user profile
-export const updateUserProfile = async (req, res) => {};
+export const updateUserProfile = async (req, res) => {
+  try {
+    // user find and check if exists
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: 'User not found!',
+      });
+    }
+
+    // destructure property
+    const { name, email, password } = req.body;
+
+    // update name
+    if (name !== undefined) {
+      user.name = name;
+    }
+
+    // update email
+    if (email !== undefined && email !== user.email) {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({
+          success: false,
+          message: 'Email already exists!',
+        });
+      }
+      user.email = email;
+    }
+
+    // update password
+    if (password !== undefined) {
+      user.password = password;
+    }
+
+    // save on db
+    const updateUser = await user.save();
+
+    // success response
+    res.status(200).json({
+      success: true,
+      message: 'Profile updated successfully',
+      user: {
+        _id: updateUser._id,
+        name: updateUser.name,
+        email: updateUser.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error!',
+      error: error.message,
+    });
+  }
+};
