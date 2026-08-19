7749b4695562d94a46c28680e59c82fec869fe6a
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-19 07:58:58 +0600

    feat: added register user middelware in auth controller file

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,5 +1,57 @@
+import User from '../models/user.model.js';
+
 // register user
-export const registerUser = async (req, res) => {};
+export const registerUser = async (req, res) => {
+  try {
+    // destructure data from req body
+    const { name, email, password, profileImageUrl, adminInviteToken } =
+      req.body;
+
+    // check user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'User already exists!' });
+    }
+
+    // detarmine user role: admin if correct token is provided, otherwise user
+    let role = 'user';
+    if (
+      adminInviteToken &&
+      adminInviteToken === process.env.ADMIN_INVITE_TOKEN
+    ) {
+      role = 'admin';
+    }
+
+    // create new user
+    const user = await User.create({
+      name,
+      email,
+      password,
+      profileImage,
+      role,
+    });
+
+    // success response
+    res.status(201).json({
+      success: true,
+      message: 'User registered successfully!',
+      data: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error!',
+      error: error.message,
+    });
+  }
+};
 
 // login user
 export const loginUser = async (req, res) => {};
