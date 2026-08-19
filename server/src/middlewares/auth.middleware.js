3edca80b63234c807976ae782b05def9f143f1d1
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-19 09:30:31 +0600

    fix: fix protect middleware issue

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -44,6 +44,7 @@ export const protect = async (req, res, next) => {
     return res.status(401).json({
       success: false,
       message: 'Token is invalid or expired!',
+      error: error.message,
     });
   }
 };
