04342a56743a83141157e4ae61f4a506b82e7ce7
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-20 18:01:35 +0600

    configure image upload checks and routes

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -6,6 +6,7 @@ import {
   updateUserProfile,
 } from '../controllers/auth.controller.js';
 import { protect } from '../middlewares/auth.middleware.js';
+import upload from '../middlewares/upload.middleware.js';
 
 const authRoute = express.Router();
 
@@ -14,5 +15,16 @@ authRoute.post('/register', registerUser); // register user
 authRoute.post('/login', loginUser); // login user
 authRoute.get('/profile', protect, getUserProfile); // get user profile
 authRoute.put('/profile', protect, updateUserProfile); // update user profile
+
+// image uploads
+authRoute.post('/upload-image', upload.single('image'), (req, res) => {
+  if (!req.file) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'No img file uploaded!' });
+  }
+  const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
+  res.status(200).json({ imageUrl });
+});
 
 export default authRoute;
