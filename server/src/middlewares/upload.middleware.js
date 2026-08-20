b114b7981680da8ebe818f7ee8ec8a0aea658c36
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-20 18:17:02 +0600

    update and implement fs and path system and fix it

--- server/src/middlewares/upload.middleware.js
+++ server/src/middlewares/upload.middleware.js
@@ -1,25 +1,43 @@
+import fs from 'fs';
 import multer from 'multer';
+import path from 'path';
+
+// uploads folders Absolute path
+const uploadDir = path.join(process.cwd(), 'uploads');
+
+// uploads folder create
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 // configure storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, uploadDir); // absolute path
   },
   filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
+    const cleanFileName = file.originalname.replace(/\s+/g, '-');
+    cb(null, `${Date.now()}-${cleanFileName}`);
   },
 });
 
 // file filter
 const fileFilter = (req, file, cb) => {
-  const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+  const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/webp'];
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
-    cb(new Error('Only .jpeg .jpg and .png formates are allowed!'), false);
+    cb(
+      new Error('Only .jpeg, .jpg, .png and .webp formats are allowed!'),
+      false,
+    );
   }
 };
 
-const upload = multer({ storage, fileFilter });
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
 
 export default upload;
