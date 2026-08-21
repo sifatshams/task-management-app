fef4dee265f0036c1480c0efd77afb78bd3e0583
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-21 09:13:54 +0600

    feat: implement create task by admin logic

--- server/src/controllers/task.controller.js
+++ server/src/controllers/task.controller.js
@@ -1,3 +1,5 @@
+import Task from '../models/task.model.js';
+
 // get all tasks (admin: all, user: only assigned tasks)
 export const getTasks = async (req, res) => {
   try {
@@ -25,6 +27,41 @@ export const getTaskById = async (req, res) => {
 // create a new task (admin only)
 export const createTask = async (req, res) => {
   try {
+    const {
+      title,
+      description,
+      priority,
+      dueDate,
+      assignedTo,
+      createdBy,
+      attachments,
+      todoCheckList,
+    } = req.body;
+
+    // validation
+    if (!Array.isArray(assignedTo)) {
+      return res.status(400).json({
+        success: false,
+        message: 'assignedTo must be an array of user IDs!',
+      });
+    }
+
+    // create task
+    const task = await Task.create({
+      title,
+      description,
+      priority,
+      dueDate,
+      assignedTo,
+      createdBy: req.user._id,
+      attachments,
+      todoCheckList,
+    });
+
+    // success response
+    res
+      .status(200)
+      .json({ success: true, message: 'Task created successfully!', task });
   } catch (error) {
     res.status(500).json({
       success: false,
