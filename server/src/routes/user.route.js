154d7b5f5c35d260597dc9f685214d4f5611100f
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-21 08:07:46 +0600

    remove delete user route

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -7,6 +7,5 @@ const userRoute = express.Router();
 // user management routes
 userRoute.get('/', protect, adminOnly, getUsers); // get users (admin only)
 userRoute.get('/:id', protect, getUserById); // get a specific user
-userRoute.delete('/:id', protect, adminOnly, deleteUser); // delete user (admin only)
 
 export default userRoute;
