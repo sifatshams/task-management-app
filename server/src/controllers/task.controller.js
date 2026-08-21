66059631be754534bfb671adc70825007f996213
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-21 08:52:04 +0600

    feat: added task controller functions

---
+++ server/src/controllers/task.controller.js
@@ -0,0 +1,83 @@
+// get all tasks (admin: all, user: only assigned tasks)
+export const getTasks = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error!',
+      error: error.message,
+    });
+  }
+};
+
+// get task by id
+export const getTaskById = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error!',
+      error: error.message,
+    });
+  }
+};
+
+// create a new task (admin only)
+export const createTask = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error!',
+      error: error.message,
+    });
+  }
+};
+
+// update task details
+export const updateTask = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error!',
+      error: error.message,
+    });
+  }
+};
+
+// delete task
+export const deleteTask = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error!',
+      error: error.message,
+    });
+  }
+};
+
+// update task status
+export const updateTaskStatus = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error!',
+      error: error.message,
+    });
+  }
+};
+
+// update task cehcklist
+export const updateTaskChecklist = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error!',
+      error: error.message,
+    });
+  }
+};
