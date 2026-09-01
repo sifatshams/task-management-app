edce24d7a57e111cd8abab28b0c81ba6eb376251
Sifat Bin Anwar <sifatshams@outlook.com>
2026-09-01 18:09:47 +0600

    feat: implement report model get export task and get export users route

---
+++ server/src/routes/report.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { adminOnly, protect } from '../middlewares/auth.middleware.js';
+
+const reportRoute = express.Router();
+
+reportRoute.get('/export/tasks', protect, adminOnly, exportTasksReport); // export all task as Excel/pdf
+reportRoute.get('/export/users', protect, adminOnly, exportUsersReport); // export users task report
+
+export default reportRoute;
