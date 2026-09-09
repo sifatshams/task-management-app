969e582441d655fe3658166e23a164b81703fa04
Sifat Bin Anwar <sifatshams@outlook.com>
2026-09-09 06:16:55 +0600

    added pass input and err or btn styles and elem

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 import Input from '../../components/Inputs/Input';
 import AuthLayout from '../../components/layouts/AuthLayout';
 
@@ -8,28 +10,52 @@ const Login = () => {
   const [error, setError] = useState(null);
 
   // handle login submit
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
   };
+
   return (
     <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
         <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+
         <p className="text-xs text-slate-700 mt-[5px] mb-6">
           Please enter your details to log in
         </p>
-      </div>
 
-      {/* form */}
-      <form onSubmit={handleLogin}>
-        <Input
-          value={email}
-          onChange={({ target }) => setEmail(target.value)}
-          label="Email Address"
-          placeholder="you@example.com"
-          type="text"
-        />
-      </form>
+        {/* form */}
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="you@example.com"
+            type="text"
+          />
+
+          <Input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder="Enter your password"
+            type="password"
+          />
+
+          {/* error msg */}
+          {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+
+          <button
+            type="submit"
+            className="w-full mt-5 cursor-pointer bg-primary hover:bg-blue-500 transition-all text-white py-3 rounded-lg"
+          >
+            Login
+          </button>
+
+          <p className='text-[13px] text-slate-800 mt-3'>Don't have an account? {" "}
+             <Link className='font-medium text-primary underline' to='/register'>SignUp</Link>
+          </p>
+        </form>
+      </div>
     </AuthLayout>
   );
 };
