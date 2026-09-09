27806b9835338f7ac57a112c3f9e6de3efde2407
Sifat Bin Anwar <sifatshams@outlook.com>
2026-09-09 06:19:00 +0600

    increate h2 text size and responsive as well

--- client/src/components/layouts/AuthLayout.jsx
+++ client/src/components/layouts/AuthLayout.jsx
@@ -5,7 +5,7 @@ const AuthLayout = ({ children }) => {
   return (
     <div className="flex">
       <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
-        <h2 className="text-lg font-medium text-black">Task Manager</h2>
+        <h2 className="text-xl lg:text-2xl font-medium text-black">Task Manager</h2>
 
         {children}
       </div>
