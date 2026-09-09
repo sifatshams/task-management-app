b3cd38f2e97d60e58966df15bb0474ee7196a647
Sifat Bin Anwar <sifatshams@outlook.com>
2026-09-09 06:25:35 +0600

    added login email and passcode validation

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -1,11 +1,82 @@
-import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import Input from '../../components/Inputs/Input';
+import AuthLayout from '../../components/layouts/AuthLayout';
+import { validateEmail } from '../../utils/helper';
 
 const Login = () => {
-  return (
-    <div>
-      Login page
-    </div>
-  )
-}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
-export default Login
+  // handle login submit
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    // validation
+    if (!validateEmail(email)) {
+      setError('Please enter a valid email!');
+      return;
+    }
+
+    if (!password) {
+      setError('Please enter the passcode!');
+    }
+
+    setError('');
+
+    // login api call
+    
+  };
+
+  return (
+    <AuthLayout>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Please enter your details to log in
+        </p>
+
+        {/* form */}
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="you@example.com"
+            type="text"
+          />
+
+          <Input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder="Enter your password"
+            type="password"
+          />
+
+          {/* error msg */}
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+          <button
+            type="submit"
+            className="w-full mt-5 cursor-pointer bg-primary hover:bg-blue-500 transition-all text-white py-3 rounded-lg"
+          >
+            Login
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            Don't have an account?{' '}
+            <Link className="font-medium text-primary underline" to="/register">
+              SignUp
+            </Link>
+          </p>
+        </form>
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default Login;
