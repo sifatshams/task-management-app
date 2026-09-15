427e284ba5bdc2687163a03c721c6095f99c44c5
Sifat Bin Anwar <sifatshams@outlook.com>
2026-09-15 21:11:19 +0600

    feat: added select users and get all users and create btn ui

---
+++ client/src/components/Inputs/SelectUsers.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react';
+import { LuUser } from 'react-icons/lu';
+import { API_PATHS } from '../../utils/api_path';
+import axiosInstance from '../../utils/axios_instance';
+
+const SelectUsers = ({ selectedUsers, setSelectedUsers }) => {
+  const [allUsers, setAllUsers] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [tempSelectedUsers, setTempSelectedUsers] = useState([]);
+
+  // get all users
+  const getAllUsers = async () => {
+    try {
+      const response = await axiosInstance.get(API_PATHS.USERS.GET_ALL_USERS);
+      if (response.data?.length > 0) {
+        setAllUsers(response.data);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const toggleUserSelection = (userId) => {
+    {
+      setTempSelectedUsers((prev) =>
+        prev.includes(userId)
+          ? prev.filter((id) => id !== userId)
+          : [...prev, userId],
+      );
+    }
+  };
+
+  const handleAssign = () => {
+    setSelectedUsers(tempSelectedUsers);
+    setIsModalOpen(false);
+  };
+
+  const selectedUserAvatars = allUsers
+    .filter((user) => selectedUsers.includes(user._id))
+    .map((user) => user.ProfileImage);
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  useEffect(() => {
+    if (selectedUsers.length === 0) {
+      setTempSelectedUsers([]);
+    }
+    return () => {};
+  }, [selectedUsers]);
+
+  return (
+    <div className="space-y-4 mt-2">
+      {selectedUserAvatars.length === 0 && (
+        <button className="card-btn" onClick={() => setIsModalOpen(true)}>
+          <LuUser className="text-sm" /> Add Members
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default SelectUsers;
