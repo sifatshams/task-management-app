e1f1901c3c99a742bbe4f0c8bfaabdb1e74bd4f6
Sifat Bin Anwar <sifatshams@outlook.com>
2026-09-24 21:32:56 +0600

    fix(client): update BASE_URL to use VITE_API_BASE_URL environment variable

--- client/src/utils/api_path.js
+++ client/src/utils/api_path.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
 
 // utils/apiPaths.js
 export const API_PATHS = {
