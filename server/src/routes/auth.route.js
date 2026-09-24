5d0916804ead0c2c7e71357ce805ac12efeb84e0
Sifat Bin Anwar <sifatshams@outlook.com>
2026-09-24 19:31:56 +0600

    feat(auth): update image upload endpoint to return Cloudinary secure URL

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { upload } from '../config/cloudinary.js';
 import {
   getUserProfile,
   loginUser,
@@ -6,7 +7,6 @@ import {
   updateUserProfile,
 } from '../controllers/auth.controller.js';
 import { protect } from '../middlewares/auth.middleware.js';
-import upload from '../middlewares/upload.middleware.js';
 
 const authRoute = express.Router();
 
@@ -16,15 +16,21 @@ authRoute.post('/login', loginUser); // login user
 authRoute.get('/profile', protect, getUserProfile); // get user profile
 authRoute.put('/profile', protect, updateUserProfile); // update user profile
 
-// image uploads
+// image uploads (using cloudinary)
 authRoute.post('/upload-image', upload.single('image'), (req, res) => {
   if (!req.file) {
     return res
       .status(400)
-      .json({ success: false, message: 'No img file uploaded!' });
+      .json({ success: false, message: 'no image file uploaded!' });
   }
-  const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
-  res.status(200).json({ imageUrl });
+
+  // req.file.path
+  const imageUrl = req.file.path;
+
+  return res.status(200).json({
+    success: true,
+    imageUrl,
+  });
 });
 
 export default authRoute;
